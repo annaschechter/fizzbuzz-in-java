38eb0aef4b9b0c3276ed16a2721149d0c5abc2dc
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-18 11:59:33 +0000

    isDivisibleByThree method complete

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -2,7 +2,7 @@
  * Created by annajerofejeva on 18/11/2014.
  */
 public class Fizzbuzz {
-    public boolean isDivisibleByThree(int i) {
-        return true;
+    public boolean isDivisibleByThree(int number) {
+        return (number % 3 == 0);
     }
 }
--- src/test/java/fizzbuzzTest.java
+++ src/test/java/fizzbuzzTest.java
@@ -1,11 +1,24 @@
+import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertTrue;
 
+
 public class fizzbuzzTest {
+    Fizzbuzz fizzbuzz;
+
+    @Before
+    public void setUp(){
+        fizzbuzz = new Fizzbuzz();
+    }
+
     @Test
     public void isDivisibleByThree() {
-        Fizzbuzz fizzbuzz = new Fizzbuzz();
         assertTrue(fizzbuzz.isDivisibleByThree(3));
+    }
+    @Test
+    public void isNotDivisibleByThree() {
+        assertFalse(fizzbuzz.isDivisibleByThree(1));
     }
 }
