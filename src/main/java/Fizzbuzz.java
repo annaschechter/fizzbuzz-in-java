90d9619e3e998b075c36b7693bc699d09296a945
Anna Jerofejeva <jerofejeva.anna@yahoo.com>
2014-11-19 08:25:47 +0000

    Added divisible by 5 and 15

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -5,4 +5,12 @@ public class Fizzbuzz {
     public boolean isDivisibleByThree(int number) {
         return (number % 3 == 0);
     }
+
+    public boolean isDivisibleByFive(int number) {
+        return (number % 5 == 0);
+    }
+
+    public boolean isDivisibleByFifteen(int number) {
+        return (number % 15 == 0);
+    }
 }
--- src/test/java/fizzbuzzTest.java
+++ src/test/java/fizzbuzzTest.java
@@ -1,3 +1,4 @@
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -20,5 +21,23 @@ public class fizzbuzzTest {
     @Test
     public void isNotDivisibleByThree() {
         assertFalse(fizzbuzz.isDivisibleByThree(1));
+    }
+
+    @Test
+    public void isDivisibleByFive() {
+        assertTrue(fizzbuzz.isDivisibleByFive(5));
+    }
+
+    @Test
+    public void isNotDivisibleByFive() {
+        assertFalse(fizzbuzz.isDivisibleByFive(1));
+    }
+    @Test
+    public void isDivisibleByFifteen() {
+        assertTrue(fizzbuzz.isDivisibleByFifteen(15));
+    }
+    @Test
+    public void isNotDivisibleByFifteen() {
+        assertFalse(fizzbuzz.isDivisibleByFifteen(1));
     }
 }
